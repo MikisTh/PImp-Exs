8c9e2dcef1550aa57a199b17c7579955d2fa3290
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-03-08 12:51:28 -0300

    Delete Circ.jsx

--- Circ.jsx
+++
@@ -1,9 +0,0 @@
-var raio = prompt("Raio do circulo: ");
-raio = parseFloat(raio);
-
-var comprimento = 2 * Math.PI * raio;
-var area = Math.PI * raio * raio;
-
-document.write("Comprimento da circunferencia        = ", comprimento, "<br>");
-document.write("Area da circunferencia               = ", area);
-
