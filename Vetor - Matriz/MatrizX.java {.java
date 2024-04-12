170fb30a36aaa2996ee5df0fb6db663a99431400
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-11 21:41:07 -0300

    Add
    Add

---
+++ Vetor - Matriz/MatrizX.java {.java
@@ -0,0 +1,22 @@
+
+
+public class MatrizX {
+    
+    public static void main (String[] args) {
+        int[][] = new int [i][j]; 
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+
+    };
+
+    for (int=0;i<mat.length;i++){
+        for(int j = 0; j <mat.length; j++) {
+            if(i==j)
+                system.out.println(mat[i][j]);
+            if(i+j == mat.length - 1)
+                System.out.println(mat[i][j]);
+        }
+
+    }
+}
