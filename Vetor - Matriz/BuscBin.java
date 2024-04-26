460ba7e1701d87f62d5af38851a9a77c19840f03
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-25 21:30:15 -0300

    Add (Alterações pendentes)
    Add

---
+++ Vetor - Matriz/BuscBin.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BuscBin {
+    public static void main (String[] args){
+        Scanner s = new Scanner(System.in);
+        int [] v = new int [2, 4, 5, 7, 10, 14, 15];
+        int x = s.nextInt(x);
+
+        for (i=0;i<n;i++){
+            if (v[i]==3)
+            System.out.println(i);
+            break;
+        }
+    
+
+    }
+}
---
+++ Vetor - Matriz/BuscSeq.java
@@ -0,0 +1,19 @@
+import java.util.scanner;
+
+public class BuscSeq {
+    public static void main (String[] args){
+        Scanner s = new Scanner(System.in);
+        int[]v = new int [6];
+        int x = s.nextInt(x);
+
+        for (i=0;i<n;i++){
+            if (v[i]==3)
+            System.out.println(i);
+            break;
+        }
+    
+    
+    
+    
+    }
+}
