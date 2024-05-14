325923687153551caede43607053480262abb11d
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-05-13 21:27:57 -0300

    Add
    Add

---
+++ Ordenação/InsertionSort.java
@@ -0,0 +1,19 @@
+public class InsertionSort {
+    public static void insertionSort (int[] v, int n) {
+        int i, j = 1;
+        int aux = 0;
+        while(j < n)
+        {
+            aux = v[j];
+            i = j - 1;
+            while ((i >= 0) && (v[i] > aux))
+            {
+                v[i + 1] = v[i];
+                i = i - 1;
+            }
+            v[i + 1] = aux;
+            j = j + 1;
+        }
+        
+    }
+}
